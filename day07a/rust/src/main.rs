21da1201c690fc3f89cd4580844cad4f858f249a
S K Y <9327767-xxSkyy@users.noreply.gitlab.com>
2024-12-07 21:03:10 +0100

    refactor: day07a rust

--- day07a/rust/src/main.rs
+++ day07a/rust/src/main.rs
@@ -8,21 +8,21 @@ const INPUT_LOCATION: &str = "./input.txt";
 fn main() {
     let input = fs::read_to_string(INPUT_LOCATION).expect("No input file found");
 
-    let mut correct_sum: isize = 0;
+    let mut correct_sum: usize = 0;
     let symbols = ["+", "*"];
 
     let line_regex = Regex::new(r"(.*\d): (.*\d)").expect("Regex issue");
 
-    let data: Vec<Vec<isize>> = input
+    let data: Vec<Vec<usize>> = input
         .trim()
         .lines()
         .map(|line| {
             let captures = line_regex.captures(line).unwrap();
-            let mut result = vec![captures[1].parse::<isize>().unwrap()];
-            let mut numbers: Vec<isize> = captures[2]
+            let mut result = vec![captures[1].parse::<usize>().unwrap()];
+            let mut numbers: Vec<usize> = captures[2]
                 .to_string()
                 .split(" ")
-                .map(|n| n.parse::<isize>().unwrap())
+                .map(|n| n.parse::<usize>().unwrap())
                 .collect();
 
             result.append(&mut numbers);
@@ -39,19 +39,17 @@ fn main() {
         let combinations = combinations_input.into_iter().multi_cartesian_product();
 
         for combination in combinations {
-            let sum: isize =
+            let sum: usize =
                 numbers
                     .clone()
                     .into_iter()
                     .enumerate()
                     .fold(0, |acc, (index, number)| {
-                        let number = isize::try_from(number).unwrap();
-
                         if acc == 0 {
                             return number;
                         };
 
-                        match combination[usize::try_from(index - 1).unwrap()] {
+                        match combination[index - 1] {
                             &"+" => acc + number,
                             _ => acc * number,
                         }
